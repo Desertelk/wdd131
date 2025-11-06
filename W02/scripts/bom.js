9f9f7c51dcf0b0572191cf59bfe264bd751e7f82
Desertelk <cadenwatts111@gmail.com>
2025-11-05 23:52:20 -0500

    Learning activities 11/5

--- W02/scripts/bom.js
+++ W02/scripts/bom.js
@@ -1,10 +1,21 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
-const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
 
-deleteButton.textContent = '❌';
-li.append(deleteButton);
+button.addEventListener('click', function() {
+    if (input.value !== '') {
+        const deleteButton = document.createElement('button');
+        const list = document.querySelector('ul');
+        const li = document.createElement('li')
+        li.textContent = input.value;
+        list.append(li);
+        input.value = '';
+        input.focus();
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+    }
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+        input.focus();
+    });
+});
