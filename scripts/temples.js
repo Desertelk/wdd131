08ca501804eb846262a02f351cfed77a5ca9a259
Desertelk <cadenwatts111@gmail.com>
2025-11-08 11:15:09 -0500

    Final changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ const modifiedElement = document.getElementById('lastModified');
 
 yearElement.textContent = new Date().getFullYear();
 modifiedElement.textContent = document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
